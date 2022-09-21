faa87baa71528298cda6234075f399b3d437c45d
Dmitrii-Naumov <46959488+Dmitrii-Naumov@users.noreply.github.com>
2022-09-21 16:37:25 -0400

    Add Shipping Extension
     that allows to send custom parameters to a request via attributes

---
+++ ShippingExtension/Dummy_SOShipmentEntryExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using PX.CarrierService;
+using PX.Data;
+using PX.Objects.SO;
+
+using Document = PX.Objects.SO.GraphExtensions.CarrierRates.Document;
+
+namespace ShippingExtension
+{
+    public class Dummy_SOShipmentEntryExtension : PXGraphExtension<SOShipmentEntry.CarrierRates, SOShipmentEntry>
+    {
+        public static bool IsActive()
+        {
+            return true;
+        }
+
+        [PXOverride]
+        public virtual CarrierRequest GetCarrierRequest(
+            Document doc,
+            UnitsType unit,
+            List<string> methods,
+            List<CarrierBoxEx> boxes,
+            Func<Document, UnitsType, List<string>, List<CarrierBoxEx>, CarrierRequest> baseMethod)
+        {
+            return baseMethod(doc, unit, methods, boxes);
+        }
+        [PXOverride]
+        public virtual CarrierRequest BuildRequest(SOShipment shiporder, Func<SOShipment, CarrierRequest> baseMethod)
+        {
+            var result = baseMethod(shiporder);
+            result.Attributes.Add($"ShipmentNbr: {shiporder.ShipmentNbr}");
+            return result;
+        }
+    }
+}
